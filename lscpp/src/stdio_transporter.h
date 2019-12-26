a3d97aaf0cd6d529fbd9f32d5641efb8c0af909e
Hannes Vogt <hannes@havogt.de>
2019-12-26 21:55:16 +0100

    Refactor stdio_transporter (#10)

--- lscpp/src/stdio_transporter.h
+++ lscpp/src/stdio_transporter.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <memory>
 #include <string>
 
 namespace lscpp {
 
 class stdio_transporter {
+private:
+  std::size_t size_ = 1024;
+  std::unique_ptr<char[]> data_{new char[size_]};
+
+  void reserve(std::size_t size);
+
 public:
   char read_char();
   std::string read_message(std::size_t length);
