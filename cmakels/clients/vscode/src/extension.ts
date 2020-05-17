405b367469d10de87f59ca1c0c3224a4f0b21df4
Hannes Vogt <hannes@havogt.de>
2020-05-17 13:40:43 +0200

    client support for target dependencies

--- cmakels/clients/vscode/src/extension.ts
+++ cmakels/clients/vscode/src/extension.ts
@@ -1,4 +1,4 @@
-import { workspace, ExtensionContext, commands, window } from 'vscode';
+import { workspace, ExtensionContext, commands, window, ViewColumn } from 'vscode';
 
 import {
 	LanguageClient,
@@ -6,7 +6,12 @@ import {
 	ServerOptions,
 	TextDocumentPositionParams,
 	TextDocumentIdentifier,
+	RequestType,
+	Range,
+	MarkupContent
 } from 'vscode-languageclient';
+
+import { GraphvizPreviewGenerator } from './vscode-graphviz/GraphvizPreviewGenerator';
 
 /**
  * Method to get workspace configuration option
@@ -20,6 +25,88 @@ function getConfig<T>(option: string, defaultValue?: any): T {
 
 
 let client: LanguageClient;
+
+
+namespace CustomDependenciesRequest {
+    export const type =
+        new RequestType<TextDocumentPositionParams, Dependencies, void, void>(
+            'custom/dependencies');
+}
+
+type DependencyNodeId = string | number;
+
+/**
+ * The result of a dependency request.
+ */
+interface Dependencies {
+	/**
+	 * The dependency graph as a list of `DependencyNode`s ...
+	 */
+	nodes: DependencyNode[];
+
+	/**
+	 * ... and a list of edges between nodes.
+	 */
+	edges: DependencyEdge[];
+
+	/**
+	 * An optional root, identifies the root node, if any.
+	 */
+	root: DependencyNodeId;
+
+	/**
+	 * An optional range is a range inside a text document
+	 * that is used to visualize a dependency request, e.g. by changing the background color.
+	 */
+	range?: Range;
+}
+
+/**
+ * A node in the dependency graph
+ */
+interface DependencyNode {
+	/**
+	 * Identifier of the node
+	 */
+	id: DependencyNodeId;
+
+	/**
+	 * The node's content.
+	 */
+	content: MarkupContent;
+}
+
+/**
+ * An edge in the dependency graph
+ */
+interface DependencyEdge {
+	/**
+	 * Parent
+	 */
+	from: DependencyNodeId;
+
+	/**
+	 * Child
+	 */
+	to: DependencyNodeId;
+
+	/**
+	 * Edge kind
+	 */
+	kind: number;
+}
+
+/**
+ * Edge kind.
+ * TODO: instead of graphical representation, the kind could be a semantic kind.
+ * But not sure if it could be expressed universal enough.
+ */
+namespace DependencyEdgeKind {
+	export const Solid = 1;
+	export const Dashed = 2;
+	export const Dotted = 3;
+	export const Bold = 4;
+}
 
 export function activate(context: ExtensionContext) {
 	let executablePath = getConfig<string>('path');
@@ -67,15 +154,39 @@ export function activate(context: ExtensionContext) {
 				textDocument: identifier,
 				position: window.activeTextEditor.selection.active};
 
-			client.sendRequest("custom/dependencies", params );
+			client.sendRequest(CustomDependenciesRequest.type, params ).then((result) => {
+				// console.log("yeah: " + result.dependencies[0].id);
+				const graphvizPreviewGenerator = new GraphvizPreviewGenerator(context);
+				let dotgraph:string = "digraph graphname {\n";
+				dotgraph += "node [color=purple]\n";
+				for (let e of result.edges) {
+					// if(d.children) {
+					// 	for(let child of d.children) {
+							dotgraph += "\"" + e.from + "\"->\"" + e.to + "\"  [color=\"blue\" style=\"";
+							switch(e.kind) {
+								case DependencyEdgeKind.Bold:
+									dotgraph += "bold";
+									break;
+								case DependencyEdgeKind.Dashed:
+									dotgraph += "dashed";
+									break;
+								case DependencyEdgeKind.Solid:
+									dotgraph += "solid";
+									break;
+								case DependencyEdgeKind.Dotted:
+									dotgraph += "dotted";
+									break;
+							}
+							dotgraph +="\"];";
+					// 	}
+					// }
+				}
+				dotgraph += "}";
+				graphvizPreviewGenerator.revealOrCreatePreview("my_target", dotgraph, ViewColumn.Beside);
+
+			});
 		}
 	}));
-
-	client.onReady().then(() => {
-		client.onRequest("custom/dependencies", () => {
-			console.log("received custom/dependencies");
-		});
-	});
 
 	client.start();
 }
---
+++ cmakels/clients/vscode/src/vscode-graphviz/GraphvizPreviewGenerator.ts
@@ -0,0 +1,192 @@
+import { ExtensionContext, TextDocument, window, ViewColumn, Uri, WebviewPanel, workspace, Disposable } from "vscode";
+const { Module, render } = require('viz.js/full.render.js');
+let Viz = require("viz.js");
+import * as path from "path";
+// import { SvgExporter } from "./SvgExporter";
+// import { OpenInBrowser } from "./OpenInBrowser";
+import { getPreviewTemplate, CONTENT_FOLDER } from "./ContentUtils";
+var fs = require("fs");
+
+export class GraphvizPreviewGenerator extends Disposable {
+
+    webviewPanels = new Map<string, PreviewPanel>();
+
+    // timeout: NodeJS.Timer;
+
+    constructor(private context: ExtensionContext) {
+        super(() => this.dispose());
+     }
+
+    // setNeedsRebuild(uri: Uri, needsRebuild: boolean): void {
+    //     let panel = this.webviewPanels.get(uri);
+
+    //     if (panel) {
+    //         panel.setNeedsRebuild(needsRebuild);
+
+    //         this.resetTimeout();
+    //     }
+    // }
+
+    // resetTimeout(): void {
+    //     if(this.timeout) {
+    //         clearTimeout(this.timeout);
+    //     }
+    //     this.timeout = setTimeout(() => this.rebuild(), 1000);
+    // }
+
+    // dispose(): void {
+    //     clearTimeout(this.timeout);
+    // }
+
+    // rebuild(): void {
+    //     this.webviewPanels.forEach(panel => {
+    //         if(panel.getNeedsRebuild() && panel.getPanel().visible)
+    //             this.updateContent(panel, workspace.textDocuments.find(doc => doc.uri == panel.uri));
+    //     });
+    // }
+
+    async revealOrCreatePreview(id: string, doc: string, displayColumn: ViewColumn): Promise<void> {
+        let previewPanel = this.webviewPanels.get(id);
+
+        if (previewPanel) {
+            previewPanel.reveal(displayColumn);
+        }
+        else {
+            previewPanel = this.createPreviewPanel(doc, displayColumn);
+            this.webviewPanels.set(id, previewPanel);
+            // when the user closes the tab, remove the panel
+            previewPanel.getPanel().onDidDispose(() => this.webviewPanels.delete(id), undefined, this.context.subscriptions);
+            // when the pane becomes visible again, refresh it
+            // previewPanel.getPanel().onDidChangeViewState(_ => this.rebuild());
+
+            // previewPanel.getPanel().webview.onDidReceiveMessage(e => this.handleMessage(previewPanel, e), undefined, this.context.subscriptions);
+        }
+
+        this.updateContent(previewPanel, doc);
+    }
+
+    handleMessage(previewPanel: PreviewPanel, message: any): void {
+        console.log(`Message received from the webview: ${message.command}`);
+
+        switch(message.command){
+            case 'scale':
+                previewPanel.setScale(message.value);
+                break;
+            case 'fitToHeight':
+                previewPanel.setFitToHeight(message.value);
+                break;
+            case 'fitToWidth':
+                previewPanel.setFitToWidth(message.value);
+                break;
+            // case 'export':
+            //     new SvgExporter().export(previewPanel.uri);
+            //     break;
+            // case 'open':
+            //     new OpenInBrowser(this.context).open(previewPanel.uri);
+            //     break;
+            default:
+                console.warn('Unexpected command: ' + message.command);
+        }
+    }
+
+    createPreviewPanel(doc: string, displayColumn: ViewColumn): PreviewPanel {
+        let previewTitle = 'Preview: '; //${path.basename(window.activeTextEditor.document.fileName)}'`;
+
+        let webViewPanel = window.createWebviewPanel('graphvizPreview', previewTitle, displayColumn, {
+            enableFindWidget: true,
+            enableScripts: true,
+            localResourceRoots: [Uri.file(path.join(this.context.extensionPath, "content"))]
+        });
+
+        webViewPanel.iconPath = Uri.file(this.context.asAbsolutePath("content/icon.svg"));
+
+        return new PreviewPanel(doc, webViewPanel);
+    }
+
+    async updateContent(previewPanel: PreviewPanel, doc: string) {
+        if(!previewPanel.getPanel().webview.html) {
+            previewPanel.getPanel().webview.html = "Please wait...";
+        }
+        // previewPanel.setNeedsRebuild(false);
+        previewPanel.getPanel().webview.html = await this.getPreviewHtml(previewPanel, doc);
+    }
+
+    toSvg(doc: string): Thenable<string> | string {
+        return new Viz({Module, render}).renderString(doc);
+    }
+
+    private async getPreviewHtml(previewPanel: PreviewPanel, doc: string): Promise<string> {
+        let templateHtml = await getPreviewTemplate(this.context, "previewTemplate.html");
+
+        // change resource URLs to vscode-resource:
+        templateHtml = templateHtml.replace(/<script src="(.+)">/g, (scriptTag, srcPath) => {
+            scriptTag;
+            let resource=Uri.file(
+                path.join(this.context.extensionPath,
+                    CONTENT_FOLDER,
+                    srcPath))
+                    .with({scheme: "vscode-resource"});
+            return `<script src="${resource}">`;
+        }).replace("initializeScale(1,false,false)",
+            `initializeScale(${previewPanel.getScale()}, ${previewPanel.getFitToWidth()}, ${previewPanel.getFitToHeight()})`);
+
+        let svg = "";
+        try {
+            svg = await this.toSvg(doc);
+        }catch(error){
+            svg = error.toString();
+        }
+
+        return templateHtml.replace("PLACEHOLDER", svg);
+    }
+}
+
+class PreviewPanel {
+
+    // needsRebuild: boolean;
+    scale = 1;
+    fitToWidth = false;
+    fitToHeight = false;
+
+    constructor(public dotString: string, private panel: WebviewPanel) {}
+
+    setScale(value: number): void {
+        this.scale = value;
+    }
+
+    getScale(): number {
+        return this.scale;
+    }
+
+    setFitToWidth(value: boolean): void {
+        this.fitToWidth = value;
+    }
+
+    getFitToWidth(): boolean {
+        return this.fitToWidth;
+    }
+
+    setFitToHeight(value: boolean): void {
+        this.fitToHeight = value;
+    }
+
+    getFitToHeight(): boolean {
+        return this.fitToHeight;
+    }
+
+    reveal(displayColumn: ViewColumn): void {
+        this.panel.reveal(displayColumn);
+    }
+
+    // setNeedsRebuild(needsRebuild: boolean) {
+    //     this.needsRebuild = needsRebuild;
+    // }
+
+    // getNeedsRebuild(): boolean {
+    //     return this.needsRebuild;
+    // }
+
+    getPanel(): WebviewPanel {
+        return this.panel;
+    }
+}
